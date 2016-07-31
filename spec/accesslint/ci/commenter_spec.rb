8949c053b1202b79f305ae6ba540e8318574f2c8
Cameron Cundiff <github@ckundo.com>
2016-07-31 16:41:26 -0400

    Fix incorrect spec assertion

--- spec/accesslint/ci/commenter_spec.rb
+++ spec/accesslint/ci/commenter_spec.rb
@@ -4,6 +4,9 @@ module Accesslint
   module Ci
     describe Commenter do
       it "posts a comment to GitHub" do
+        original_pull_request = ENV["CI_PULL_REQUEST"]
+        ENV["CI_PULL_REQUEST"] = "https://github.com/accesslint/accesslint-ci.rb/pull/9001"
+
         allow(RestClient).to receive(:get).
           with("https://accesslint-ci:ABC456@api.github.com/repos/accesslint/accesslint-ci.rb/pulls?head=accesslint:#{ENV.fetch('CIRCLE_BRANCH')}").
           and_return([ { number: 1 } ].to_json)
@@ -12,15 +15,17 @@ module Accesslint
         Commenter.perform(["my diff"])
 
         expect(RestClient).to have_received(:post).with(
-          "https://accesslint-ci:ABC456@api.github.com/repos/accesslint/accesslint-ci.rb/issues/1/comments",
+          "https://accesslint-ci:ABC456@api.github.com/repos/accesslint/accesslint-ci.rb/issues/9001/comments",
           { body: "Found 1 new accessibility issues: \n```\nmy diff\n```" }.to_json
         )
+
+        ENV["CI_PULL_REQUEST"] = original_pull_request
       end
 
       context "when CI_PULL_REQUEST is set" do
         it "uses the number from CI_PULL_REQUEST" do
+          original_pull_request = ENV["CI_PULL_REQUEST"]
           ENV["CI_PULL_REQUEST"] = "https://github.com/accesslint/accesslint-ci.rb/pull/9001"
-
           allow(RestClient).to receive(:post)
 
           Commenter.perform(["my diff"])
@@ -30,14 +35,14 @@ module Accesslint
             { body: "Found 1 new accessibility issues: \n```\nmy diff\n```" }.to_json
           )
 
-          ENV.delete("CI_PULL_REQUEST")
+          ENV["CI_PULL_REQUEST"] = original_pull_request
         end
       end
 
       context "when running on master" do
         it "uses the number from CI_PULL_REQUEST" do
+          original_pull_request = ENV["CI_PULL_REQUEST"]
           ENV["CI_PULL_REQUEST"] = "https://github.com/accesslint/accesslint-ci.rb/pull/9001"
-
           allow(RestClient).to receive(:post)
 
           Commenter.perform(["my diff"])
@@ -47,7 +52,7 @@ module Accesslint
             { body: "Found 1 new accessibility issues: \n```\nmy diff\n```" }.to_json
           )
 
-          ENV.delete("CI_PULL_REQUEST")
+          ENV["CI_PULL_REQUEST"] = original_pull_request
         end
       end
     end
