d1fccc9a43dccac551da3074d93d76970434e47a
Cameron Cundiff <ccundiff@etsy.com>
2016-12-18 17:12:54 -0500

    Circle Token is not required for public repos
    - In keeping with https://github.com/accesslint/accesslint-ci/issues/40#issuecomment-267849843.

--- spec/accesslint/ci/log_manager_spec.rb
+++ spec/accesslint/ci/log_manager_spec.rb
@@ -7,11 +7,11 @@ module Accesslint
         context "with existing logs from a master build" do
           it "returns the existing logs" do
             allow(RestClient).to receive(:get).
-              with("https://example.com/accesslint.log?circle-token=ABC123").
+              with("https://example.com/accesslint.log").
               and_return("1\n2\n")
 
             allow(RestClient).to receive(:get).
-              with("https://circleci.com/api/v1/project/accesslint/accesslint-ci/latest/artifacts?branch=master&filter=successful&circle-token=ABC123").
+              with("https://circleci.com/api/v1/project/accesslint/accesslint-ci/latest/artifacts?branch=master&filter=successful").
               and_return([
                 path: "accesslint.log",
                 url: "https://example.com/accesslint.log",
@@ -26,7 +26,7 @@ module Accesslint
         context "with no existing logs" do
           it "returns a newline" do
             allow(RestClient).to receive(:get).
-              with("https://circleci.com/api/v1/project/accesslint/accesslint-ci/latest/artifacts?branch=master&filter=successful&circle-token=ABC123").
+              with("https://circleci.com/api/v1/project/accesslint/accesslint-ci/latest/artifacts?branch=master&filter=successful").
               and_return([].to_json)
 
             log = LogManager.get
@@ -38,7 +38,7 @@ module Accesslint
         context "with no existing artifacts" do
           it "returns a newline" do
             allow(RestClient).to receive(:get).
-              with("https://circleci.com/api/v1/project/accesslint/accesslint-ci/latest/artifacts?branch=master&filter=successful&circle-token=ABC123").
+              with("https://circleci.com/api/v1/project/accesslint/accesslint-ci/latest/artifacts?branch=master&filter=successful").
               and_raise(RestClient::NotFound.new)
 
             log = LogManager.get
