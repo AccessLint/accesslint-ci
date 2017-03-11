ad89782cd2f9ee8bfa15ba2f201441f5989cee88
Cameron Cundiff <github@ckundo.com>
2017-03-11 11:30:22 -0500

    Add an option for hosted accesslint platform
    - When running on accesslint servers, we still want to comment but we
      don't want to try to fetch circle artifacts.

--- lib/accesslint/ci/cli.rb
+++ lib/accesslint/ci/cli.rb
@@ -5,20 +5,26 @@ module Accesslint
     class Cli < Thor
       desc "scan HOST", "scan HOST"
       option :"skip-ci", type: :boolean
+      option :"hosted", type: :boolean
       def scan(host)
         current = Scanner.perform(host: host).split("\n")
 
         if !options[:"skip-ci"]
+          existing = []
+
           if ENV.fetch("CIRCLE_BRANCH") != "master"
-            existing = LogManager.get.split("\n")
+            if !options[:"hosted"]
+              existing = LogManager.get.split("\n")
+            end
+
             diff = current - existing
 
             if diff.any?
               Commenter.perform(diff)
             end
-
-            puts diff
           end
+
+          puts diff
         else
           puts current
         end
